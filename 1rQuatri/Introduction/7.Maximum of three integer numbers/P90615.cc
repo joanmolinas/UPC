80c44f1ca1ad8f9b6c891236d2d17922ef34f4b6
Joan Molinas Ramon <joan@MacBook-MacBook-Pro-de-Joan.local>
2015-09-24 09:57:38 +0200

    add problem 7

---
+++ 1rQuatri/Introduction/7.Maximum of three integer numbers/P90615.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n1, n2, n3, maxim;
+  
+  cin>>n1>>n2>>n3;
+
+  maxim = max(n1, n2);
+  maxim = max(maxim, n3);
+  cout<<maxim<<endl;
+
+  return 0;
+}
