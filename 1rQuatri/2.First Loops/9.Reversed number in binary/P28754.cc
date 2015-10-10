f6a44c98d0d141f371e54f192ec4c8af0612352d
Joan Molinas Ramon <joan@MacBook-MacBook-Pro-de-Joan.local>
2015-10-10 15:10:02 +0200

    add problem 9 loops

---
+++ 1rQuatri/2.First Loops/9.Reversed number in binary/P28754.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    //Pre: Input consists of a natural number
+    //Post: Print, reversed, the binary representation of the number, with as many zeros at its left as required.
+
+    int number;
+    cin>>number;
+
+    string numberString;
+
+    if(number == 0) { numberString = "0"; }
+
+    while(number>0) {
+        numberString += to_string(number%2);
+        number/=2;
+    }
+
+    cout<<numberString<<endl;
+    return 0;
+}
